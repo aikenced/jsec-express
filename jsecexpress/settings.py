1651491121d88919fbf3b1358ab477563da0dd65
Daniel Aiken Eduardo <aikenced@Daniels-MacBook-Air.local>
2025-07-18 00:25:04 +0800

    settings

--- jsecexpress/settings.py
+++ jsecexpress/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-dev-secret-key")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
